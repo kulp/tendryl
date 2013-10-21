5ae1ea47ba7fb47e669692343fa69d4d0c6fae73
Darren Kulp <darren@kulp.ch>
2013-10-20 20:41:33 -0500

    Add a main() and a try-catch to Test

--- Test.java
+++ Test.java
@@ -5,5 +5,14 @@ public class Test {
     long x = 0xdeadbeefcafeL;
     double y = 123.456D;
     float z = 1.25F;
+
+    public static void main(String[] args) {
+        try {
+            throw new Exception("test exception");
+        } catch (Exception e) {
+            System.out.println("caught " + e.getMessage());
+        }
+    }
 }
 
+/* vi: set et ts=4 sw=4: */
